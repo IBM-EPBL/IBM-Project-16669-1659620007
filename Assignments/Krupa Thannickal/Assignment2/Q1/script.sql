48eff42a1beccad42557d517349bbec7939a0fe6
KrupaET <92843413+KrupaET@users.noreply.github.com>
2022-10-12 18:45:44 +0530

    Update script.sql

--- Assignments/Krupa Thannickal/Assignment2/Q1/script.sql
+++ Assignments/Krupa Thannickal/Assignment2/Q1/script.sql
@@ -1,8 +1,12 @@
 create table USER(
-  email varchar(30),
-  username varchar(20),
-  rollno varchar(4),
-  passwd varchar(20)
+	email varchar(30),
+	username varchar(10),
+	rollno varchar(4),
+	passwd varchar(20)
 );
 
-insert into USER values('anil', 
+insert into user values('anil@gmail.com', 'anil123', 'S001', 'anilpwd');
+insert into user values('beena@gmail.com', 'beena333', 'S002', 'beenapwd');
+insert into user values('cherry@gmail.com', 'cherry007', 'S003', 'cherrypwd');
+insert into user values('donna@gmail.com', 'donna789', 'S004', 'donnapwd');
+insert into user values('elias@gmail.com', 'elias456', 'S005', 'eliaspwd');
