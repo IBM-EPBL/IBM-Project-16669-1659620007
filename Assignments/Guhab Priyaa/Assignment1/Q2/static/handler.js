dc36fa671153c9865ca28fe103ad4d5d8b4ccf6a
Priyaa511 <113786955+Priyaa511@users.noreply.github.com>
2022-10-15 19:00:49 +0530

    Create handler.js

---
+++ Assignments/Guhab Priyaa/Assignment1/Q2/static/handler.js
@@ -0,0 +1,4 @@
+function formSub(){
+    document.writeln("<br><br><center><h3>Your feedback has been received!</h3>")
+    document.writeln("<br><br><a href='/'>Return to Home Page</a></center>")
+}
